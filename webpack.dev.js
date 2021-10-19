4d51b01bc48e1125ec8fd12d0dc9ca28a7c43e64
Miguel González <migpalg@gmail.com>
2021-10-19 10:34:14 -0500

    feat(app): added eval-source-map devtool to webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const config = require('./webpack.config');
 module.exports = {
   ...config,
   mode: 'development',
+  devtool: 'eval-source-map',
   devServer: {
     compress: true,
     port: 4200,
