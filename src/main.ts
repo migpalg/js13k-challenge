37b4e478c83f587744e73e64ac787b8727b9ebc9
Miguel González <migpalg@gmail.com>
2021-10-19 10:28:20 -0500

    feat(app): added hot module reload with webpack dev server
    Added webpack-dev-server package to enable hot module reload into the project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { Coords } from '@core/rendering';
 
 function main() {
+  const app = document.getElementById('app');
   const coords = new Coords();
 
-  console.log(coords.toObject());
+  if (app)
+    app.innerHTML = `
+    <code>
+      <pre>${JSON.stringify(coords.toObject(), null, 2)}</pre>
+    </code>
+    <h1>This module is hot reloading!</h1>
+    <p>This is something to hot reload!</p>
+  `;
 }
 
 main();
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,4 +3,8 @@ const config = require('./webpack.config');
 module.exports = {
   ...config,
   mode: 'development',
+  devServer: {
+    compress: true,
+    port: 4200,
+  },
 };
